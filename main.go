fb1c76a47004699e7eaac60a635e0d8e5cdf28ee
Jakub Madajczak <jkbmdk@gmail.com>
2020-06-28 23:44:13 +0200

    pick random criminal

--- main.go
+++ main.go
@@ -4,31 +4,38 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
+	"math/rand"
+	"time"
+	"io/ioutil"
 )
+
+type Gangsters struct {
+	Gangsters []Gangster `json:"gangsters"`
+}
+
+type Gangster struct {
+	Name string `json:"name"`
+	Description string `json:"description"`
+	Price string `json:"price"`
+	Photo string `json:"photo"`
+}
+
+func randomGangster() Gangster {
+	data, _ := ioutil.ReadFile("gangsters.json")
+	var gangsters Gangsters
+	json.Unmarshal([]byte(data), &gangsters)
+	rand.Seed(time.Now().Unix())
+	return gangsters.Gangsters[rand.Intn(len(gangsters.Gangsters))]
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"gangsters": [
-		{
-			"name": "Mruczek",
-			"description": "Szef kociej mafii, poszukiwany listem gończym. Skazany za handel niecertyfikowaną karmą na czarnym rynku.",
-			"price": "$1000",
-			"photo": "https://i.ibb.co/rbgVDQ0/mruczek.jpg"
-		},
-		{
-			"name": "Puszek",
-			"description": "Lokalny raper, od czasu ataku na funkcjonariusza policji, w trakcie przeszukania, przebywa w bliżej nieokreślonym podziemiu.",
-			"price": "$400",
-			"photo": "https://i.ibb.co/mHs5wFW/puszek.png"
-		},
-		{
-			"name": "Bambam i Kicio",
-			"description": "Bracia. Należą do żylety lokalnego klubu sportowego FC Meow Meow. Obaj poszukiwani za przeprowadzenie akcji \"strącenie\" polegającej na zrzucaniu przemdiotów z półek.",
-			"price": "$700",
-			"photo": "https://i.ibb.co/1zXX2S8/bambamikicio.jp://i.ibb.co/1zXX2S8/bambamikicio.jpg"
-		}
-	]}`))
+	data, err := json.Marshal(randomGangster())
+	if err == nil {
+		w.Write(data)
+	}
 }
 
 func main() {
